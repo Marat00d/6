6c763178bfdc1f31e4f69e5fb970e97460afb96b
Davletov Ponomarev <marat@list.ru>
2022-05-16 08:45:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Текущая дата: ");
+            Console.Write(DateTime.Now);
+            Console.Write("\n");
+            Console.Write($"Текущая дата: {DateTime.Now}\n");
+        }
+    }
+}
